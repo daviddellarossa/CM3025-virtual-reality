e5cf55cbb51eff6cfd56b4b84f75a243a0a58668
David Della Rossa <david.dellarossa@gmail.com>
2021-11-17 10:29:45 +0000

    Added state machine to GameManager

---
+++ SimonVR/Assets/Scripts/GameManager/StateMachine/State.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace SimonVR.Assets.Scripts.GameManager.StateMachine
+{
+    public abstract class State
+    {
+        public abstract event EventHandler<State> ChangeStateRequestEvent;
+        public GameManager GameManager { get; protected set; }
+
+        public State(GameManager gameManager)
+        {
+            this.GameManager = gameManager;
+        }
+
+        public virtual void OnEnter()
+        {
+            Debug.Log($"Entering { this.GetType().Name} state");
+        }
+
+        public virtual void OnExit()
+        {
+            Debug.Log($"Exiting { this.GetType().Name} state");
+        }
+    }
+}
