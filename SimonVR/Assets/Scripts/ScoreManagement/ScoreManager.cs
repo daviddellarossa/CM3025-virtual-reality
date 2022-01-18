4859701334b24a4a292cf9a4a63cc8f9e96658ca
David Della Rossa <david.dellarossa@gmail.com>
2022-01-18 09:10:13 +0000

    ScoreManager connected with score display panels

--- SimonVR/Assets/Scripts/ScoreManagement/ScoreManager.cs
+++ SimonVR/Assets/Scripts/ScoreManagement/ScoreManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 namespace SimonVR.Assets.Scripts.ScoreManagement
@@ -12,11 +13,42 @@ namespace SimonVR.Assets.Scripts.ScoreManagement
         public Score CurrentScore;
         public HighScoreRecorder HighScores;
 
+        [SerializeField]
+        private GameObject HighScoreDisplayGO;
+        [SerializeField]
+        private GameObject ScoreDisplayGO;
+
+        private TextMeshProUGUI ScoreDisplay;
+        private TextMeshProUGUI HighScoreDisplay;
+
+        
+
         void Start()
         {
+            ScoreDisplay = ScoreDisplayGO.GetComponent<TextMeshProUGUI>();
+            HighScoreDisplay = HighScoreDisplayGO.GetComponent<TextMeshProUGUI>();
+
             ResetCurrentScore();
+            UpdateHighScorePanel();
+            //Valve.VR.SteamVR.instance.overlay.ShowKeyboard(0, 0, "Description", 256, "", true);
         }
 
+        private void UpdateHighScorePanel()
+        {
+            if(HighScoreDisplay != null)
+            {
+                var highScore = HighScores.HighScores.OrderByDescending(x => x.Value).FirstOrDefault();
+                HighScoreDisplay.text = highScore?.Value.ToString() ?? "0";
+            }
+        }
+
+        private void UpdateScorePanel(Score score)
+        {
+            if (ScoreDisplay != null)
+            {
+                ScoreDisplay.text = score.Value.ToString();
+            }
+        }
 
         public void AddToHighScore()
         {
@@ -27,22 +59,26 @@ namespace SimonVR.Assets.Scripts.ScoreManagement
             CurrentScore.Date = DateTime.Now.ToString("s");
             CurrentScore.Name = "DDR";
             HighScores.HighScores.Add(CurrentScore);
+            UpdateHighScorePanel();
         }
 
         public void AddToScore(int score)
         {
             CurrentScore.Value += score;
             Debug.Log($"Adding score {score}");
+            UpdateScorePanel(CurrentScore);
         }
 
         public void ResetCurrentScore()
         {
             CurrentScore = new Score();
+            UpdateScorePanel(CurrentScore);
         }
 
         public void ResetHighScore()
         {
             HighScores.HighScores.Clear();
+            UpdateHighScorePanel();
         }
     }
 }
